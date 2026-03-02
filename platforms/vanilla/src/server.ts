7441349c5acd30f820b53d326e3ff60ee1d7f62d
Robby Ranshous <rranshous@gmail.com>
2026-03-02 07:54:45 -0500

    update vanilla max body size to 200k

--- platforms/vanilla/src/server.ts
+++ platforms/vanilla/src/server.ts
@@ -25,7 +25,7 @@ app.use(cors({
   origin: true,
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '200kb' }));
 
 // Session middleware (BEFORE passport)
 const SQLiteStore = require('connect-sqlite3')(session);
